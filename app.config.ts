6d51aea4d8bc92724bfa5624ed0d8238a4fd7674
Aton Zaides <anton@taranis.ag>
2021-07-04 10:56:45 +0300

    Fixed eslint errors

--- app.config.ts
+++ app.config.ts
@@ -1,40 +1,38 @@
 export default {
-    name: "react-native-compare-slider",
-    slug: "react-native-compare-slider",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/images/icon.png",
-    scheme: "myapp",
-    userInterfaceStyle: "automatic",
-    splash: {
-        "image": "./assets/images/splash.png",
-        "resizeMode": "contain",
-        "backgroundColor": "#ffffff"
+  name: 'react-native-compare-slider',
+  slug: 'react-native-compare-slider',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/images/icon.png',
+  scheme: 'myapp',
+  userInterfaceStyle: 'automatic',
+  splash: {
+    image: './assets/images/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  web: {
+    favicon: './assets/images/favicon.png',
+  },
+  ios: {
+    config: {
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY_IOS,
     },
-    updates: {
-        "fallbackToCacheTimeout": 0
-    },
-    assetBundlePatterns: [
-        "**/*"
-    ],    
-    web: {
-        "favicon": "./assets/images/favicon.png"
-    },
-    ios: {
-      config: {
-        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY_IOS
+    supportsTablet: true,
+  },
+  android: {
+    config: {
+      googleMaps: {
+        apiKey: process.env.GOOGLE_MAPS_API_KEY_ANDROID,
       },
-      supportsTablet: true
     },
-    android: {
-      config: {
-        googleMaps: {
-          apiKey: process.env.GOOGLE_MAPS_API_KEY_ANDROID
-        }
-      },
-      adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
-        backgroundColor: "#ffffff"
-      }
+    adaptiveIcon: {
+      foregroundImage: './assets/images/adaptive-icon.png',
+      backgroundColor: '#ffffff',
     },
-}
+  },
+};
